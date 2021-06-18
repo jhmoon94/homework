101cd39f5421aeaaae79abb267045e8fa8cf18e9
jhmoon94 <jhmoon94@yahoo.com>
2021-06-18 15:14:37 -0700

    added stop sign

--- turtle_graphics/turtleGraphics.js
+++ turtle_graphics/turtleGraphics.js
@@ -1,24 +1,34 @@
-const w = '\u26AA'
-const b = '\u26AB'
 
 
 class Turtle {// x and y refers to current location and xDirection and yDirection refers to vector 
     constructor(x, y) {
+    this.w = '\u26AA'
+    this.b = '\u26AB'
+    this.s = '\u26D4'
     this.x = x;
     this.y = y;
     this.xDirection = 1;
     this.yDirection = 0;
     this.array = [[x, y]];
+    this.stuckedArray =[];//stucked location before entering out of range 
     }
     forward(n) {// Forward upon where it heads, x direction or y direction
         if (this.xDirection != 0) {
             for ( let i = 0; i < n; i ++) {// x direction is toward rightside
             this.x += this.xDirection 
+            if (this.x < 0) {
+                this.x = 0;
+                this.stuckedArray.push([this.x, this.y]);
+            }
             this.array.push([this.x, this.y]);// this is for footstamp
             }
         } else {
             for ( let i = 0; i < n; i ++) {
                 this.y -= this.yDirection // y direction is toward downside
+                if (this.y < 0) {
+                    this.y = 0;
+                    this.stuckedArray.push([this.x, this.y]);//push stucked location to stuckedArray
+                }
                 this.array.push([this.x, this.y]);
                 }
         }
@@ -48,12 +58,19 @@ class Turtle {// x and y refers to current location and xDirection and yDirectio
         for ( let i = 0; i < height; i++) {
             let tempArr = [];
             for (let j = 0; j < width; j++ ) {
-            tempArr.push(w);
+            tempArr.push(this.w);
             }
         background.push(tempArr);
         }
+
         this.array.forEach ( element => {// Change value of the path of background
-            background[element[1]][element[0]] = b;
+            background[element[1]][element[0]] = this.b;
+            this.stuckedArray.forEach ( stucked => {//because array is reference type, array's elements need to be compared by itself
+                if (stucked[0] == element[0] && stucked[1] == element[1]) 
+                    background[element[1]][element[0]] = this.s;
+                
+            }) 
+           
         })
         for (let i = 0; i< background.length; i++) {
                background[i] = background[i].join(' ');
@@ -67,8 +84,11 @@ new Turtle(0, 16)// Drawing Codecore's C
     .forward(0)
     .left()
     
-    .forward(12)
+    .forward(116)// it attemps to goes out of range hundred of times
     .right()
+    .right()
+    .forward(4)
+    .left()
 
     .forward(15)
     .right()
